ea27af573075743e920845d42ff80dfde9c658ae
ahmednaggar19 <ahmednagar2011@gmail.com>
2019-02-05 15:26:02 +0200

    fixed indentation

--- video_processing/video_utils.py
+++ video_processing/video_utils.py
@@ -10,15 +10,16 @@ def getCapturedFrames(video_filename):
 
     captured_frames = []
     while success:
-		# cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
-       	success,image = vidcap.read()
-       	# print('Read a new frame: ', count, success)
-	   	if(randint(1, frames_count) <= constant.SIZE_OF_CAPTURED_FRAMES):
-			captured_frames.append(image)
-		   	print("captured_frame #:", len(captured_frame))
+  	  # cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
+     	success,image = vidcap.read()
+     	# print('Read a new frame: ', count, success)
+     	if(randint(1, frames_count) <= constant.SIZE_OF_CAPTURED_FRAMES):
+  		captured_frames.append(image)
+  	   	print("captured_frame #:", len(captured_frame))
 
-return captured_frames
+    return captured_frames
 
 
 file_path = config.PATH + "SampleVideo_1280x720_1mb.mp4"
 getCapturedFrames(file_path)
+# to stage
