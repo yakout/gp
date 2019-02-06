2352319fa064f11fcc4b4a3cfe9b259103829cbd
Ahmed Abdellah <ahmedabdela00000000@gmail.com>
2019-02-06 13:31:48 +0200

    labeling class

---
+++ video_processing/Logo.py
@@ -0,0 +1,23 @@
+import numpy as np
+import constant
+
+class Logo(object):
+    """docstring for Logo."""
+    x1 = y1 = x2 = y2 = -1
+    logo = []
+    def __init__(self, x1, y1, x2, y2, logo):
+        super(Logo, self).__init__()
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.logo = logo
+        return
+
+    def hasLogo(self, img):
+        diff = 0
+        for i in range(self.x1, self.x2+1):
+            for j in range(self.y1, self.y2+1):
+                diff += not np.all(img[i][j] == self.logo[i][j])
+
+        return diff <= constant.LOGO_DIFF_THRESHOLD*(self.x2-self.x1+1)*(self.y2-self.y1+1)
