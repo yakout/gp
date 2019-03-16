73db74e7116adf8196cff54973b2a4b781d7cf72
ahmednaggar19 <ahmednagar2011@gmail.com>
2019-03-16 21:59:23 +0200

    reviewed Component Container in component.py

--- component/component.py
+++ component/component.py
@@ -1,8 +1,13 @@
+from abc import ABC, abstractmethod
+
 class Component():
 
+	@abstractmethod
 	def get_highlights(chunk):
+		"""
+			gets highlights for this video chunk as per this component perspective.
+			"""
 		pass
-
 
 class ComponentContainer():
 
@@ -10,10 +15,16 @@ class ComponentContainer():
 
 	@staticmethod
 	def register_component(component_name, component):
+		"""
+			registers the given component with the given name in the container.
+			"""
 		components_dict[component_name] = component
 
 	@staticmethod
 	def get_chunk_highlights(chunk):
+		"""
+			gets chunk highlights from each component and stores in a dict.
+			"""
 		result_dict = {}
 		for component_name, component in components_dict.items():
 			highlights = component.get_highlights(chunk)
