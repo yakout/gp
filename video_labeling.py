b259cd441f54dc4aee06d3ef3dcb0a8afb41d081
Ahmed Abdellah <ahmedabdela00000000@gmail.com>
2019-04-29 23:28:33 +0300

    fixing labeling issue

--- video_labeling.py
+++ video_labeling.py
@@ -1,7 +1,8 @@
 from video_processing import VideoChunkReader
 import config
-from moviepy.editor import VideoFileClip, concatenate_videoclips
+from moviepy.editor import *
 import time
+import cv2
 
 video_name = "egy-uru-0-1"
 
@@ -14,24 +15,32 @@ f = open(config.TRAINNING_OUTPUT_PATH + video_name + ".txt", "w")
 shot_length = 0
 while(video_reader.has_next()):
     chunk = video_reader.get_next()
-    # newclip = chunk.get_clip().fl_time(lambda: 3-t)
-    chunk.get_clip().preview()
+
     # if(shot_length < 10):
     #     shot_length += chunk.get_clip().duration
     #     continue
     # else:
     #     shot_length = 0
+
+    type = -1
     count += 1
 
     print("clip count: ", count)
-    type = -1
-    # while(type != 0 and type != 1):
-    #     type = int(input("Enter a type of clip: "))
-    #     # print(type(type))
-    #     if(type != 0 and type != 1):
-    #         print("y3l2 d5l 0 or 1 :D")
-    time.sleep(1)
-    # f.write(str(type) + "\n")
+
+    if(chunk.get_frames_count() >= 2):
+        chunk.get_clip().preview()
+    else:
+        type = 0
+
+    while(type != 0 and type != 1):
+        type = int(input("Enter a type of clip: "))
+        # print(type(type))
+        if(type != 0 and type != 1):
+            print("y3l2 d5l 0 or 1 :D")
+
+    # time.sleep(3)
+    f.write(str(type) + "\n")
+
 
 
 f.close()
