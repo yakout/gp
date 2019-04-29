c0eec91e298f1c93699c3ba426fd97d614722120
Ahmed Abdellah <ahmedabdela00000000@gmail.com>
2019-04-30 01:39:57 +0300

    adding three matches highlightes

--- video_labeling.py
+++ video_labeling.py
@@ -4,7 +4,7 @@ from moviepy.editor import *
 import time
 import cv2
 
-video_name = "egy-uru-0-1"
+video_name = "barca - bayrn  3 - 0"
 
 video_reader = VideoChunkReader(config.INPUT_PATH + video_name + ".mp4", 0)
 
@@ -33,7 +33,11 @@ while(video_reader.has_next()):
         type = 0
 
     while(type != 0 and type != 1):
-        type = int(input("Enter a type of clip: "))
+        try:
+            type = int(input("Enter a type of clip: "))
+        except:
+            type = -1
+
         # print(type(type))
         if(type != 0 and type != 1):
             print("y3l2 d5l 0 or 1 :D")
