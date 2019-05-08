d5a32968ef7a20bdb1406c7d46753ffb4b906748
ahmednaggar19 <ahmednagar2011@gmail.com>
2019-05-08 03:25:46 +0200

    model for new dataset built with 85% accuracy

--- general_highlights/replay_detection/video_processing/model_trainer/models_archive.py
+++ general_highlights/replay_detection/video_processing/model_trainer/models_archive.py
@@ -6,14 +6,22 @@ It makes it easier to roll back to previous models.
 Each model's listed here has the best architecture after fine tuning and
 hyperparameter tuning.
 """
-# Nueral Network Model
-NN_model = Sequential()
-NN_model.add(Dense(32, input_shape=(self.all_data.shape[1] - 1,), activation='relu'))
-NN_model.add(Dense(32, activation='relu'))
-NN_model.add(Dense(32, activation='relu'))
-NN_model.add(Dense(1, activation='sigmoid'))
-NN_model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
-# Recurrent Neural Network Model
+# Nueral Network Model 78%
+model = Sequential()
+model.add(Dense(32, input_shape=(self.all_data.shape[1] - 1,), activation='relu'))
+model.add(Dense(64, activation='tanh'))
+model.add(Dropout(0.2))
+model.add(Dense(128, activation='tanh'))
+model.add(Dropout(0.4))
+model.add(Dense(64, activation='tanh'))
+model.add(Dropout(0.6))
+model.add(Dense(32, activation='tanh'))
+model.add(Dropout(0.6))
+model.add(Dense(16, activation='tanh'))
+model.add(Dropout(0.2))
+model.add(Dense(8, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(1, activation='sigmoid'))# Recurrent Neural Network Model
 RNN_model = Sequential()
 RNN_model.add(LSTM(20, input_shape=(10, self.all_data.shape[1]), return_sequences=True))
 RNN_model.add(TimeDistributed(Dense(1, activation='sigmoid')))
--- general_highlights/replay_detection/video_processing/model_trainer/trainer.py
+++ general_highlights/replay_detection/video_processing/model_trainer/trainer.py
@@ -2,7 +2,9 @@ import numpy as np
 
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import Adam
+
 
 from sklearn.model_selection import train_test_split
 from utils import DatasetReader
@@ -38,30 +40,60 @@ class Trainer:
     """
     def __init__(self, dataset_dir):
         self.all_data = self.read_data(dataset_dir)
-        print(self.all_data.shape)
+        self.preprocess_data()
 
     def read_data(self, dataset_dir):
         reader = DatasetReader(dataset_dir)
         return reader.read_data()
 
+    def fill_nans(self):
+        nan_means = np.nanmean(self.all_data, axis=0)
+        nan_indices = np.argwhere(np.isnan(self.all_data))
+        for nan_index in nan_indices:
+            i, j = nan_index[0], nan_index[1]
+            self.all_data[i,j] = nan_means[j]
+
+    def normalize(self):
+        means = self.all_data.mean(axis=0)
+        std = self.all_data.std(axis=0)
+        self.all_data[:, :-1] -= means[:-1]
+        self.all_data[:, :-1] /= std[:-1]
+
+    def preprocess_data(self):
+        self.fill_nans()
+        self.normalize()
+
     def partition_dataset(self):
         X = self.all_data[:, :-1]
         y = self.all_data[:, -1]
         X_train, X_test, y_train, y_test = train_test_split(X, y,
-                test_size=0.2, random_state=42)
-        X_dev, X_test, y_dev, y_test = train_test_split(X, y,
+                test_size=0.1, random_state=42)
+        X_dev, X_test, y_dev, y_test = train_test_split(X_test, y_test,
                 test_size=0.5, random_state=42)
         return X_train, X_dev, X_test, y_train, y_dev, y_test
 
     def create_model(self):
         # Model Architecture
         model = Sequential()
-        model.add(Dense(32, input_shape=(self.all_data.shape[1] - 1,), activation='relu'))
+        model.add(Dense(128, input_shape=(self.all_data.shape[1] - 1,), activation='relu'))
+        model.add(Dropout(0.1))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.3))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.6))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.6))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.6))
+        model.add(Dense(64, activation='relu'))
+        model.add(Dropout(0.2))
         model.add(Dense(32, activation='relu'))
-        model.add(Dense(32, activation='relu'))
+        model.add(Dropout(0.2))
         model.add(Dense(1, activation='sigmoid'))
         # Model Config
-        model.compile(optimizer='adam',
+        model.compile(optimizer=Adam(lr=0.00005),
               loss='binary_crossentropy',
               metrics=['accuracy'])
         return model
@@ -78,10 +110,11 @@ class Trainer:
         X_train, X_dev, X_test, y_train, y_dev, y_test = self.partition_dataset()
         # ceate and train model
         model = self.create_model()
-        model.fit(X_train, y_train,  validation_data=(X_dev, y_dev), epochs=300)
+        model.fit(X_train, y_train,  validation_data=(X_dev, y_dev), epochs=100)
         print(model.evaluate(X_test, y_test))
-        self.save_model(model)
+        if (save_model):
+            self.save_model(model)
 
 if __name__ == '__main__':
     trainer = Trainer("./train_data/")
-    trainer.train()
+    trainer.train(save_model=True)
